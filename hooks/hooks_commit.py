36d5889796bb946293e3e7b6f104cfcd259cb7cc
Alvarez Mickael <alvarez@b10p24.fil.univ-lille1.fr>
2016-10-05 15:06:01 +0200

    update

--- hooks/hooks_commit.py
+++ hooks/hooks_commit.py
@@ -1,9 +1,24 @@
 from hooks_util import *
+from hooks_files import *
+from hooks_declare import *
 
 def commit_hook(argv):	
 	#execute_git_cmd(argv, False)
 	post_commit()
 	
 def post_commit():
+	status = ""
+	sha1 = ""
+	branch_name = ""
+	message = ""
+	
 	status = get_diff_between_two_last_commit()
-	print(status)
+	branch_name = execute_cmd( [ git_cmd, "rev-parse", "--abbrev-ref", "HEAD"], print_it=False ).strip()
+	log_res = execute_cmd( [ git_cmd, "log",  "--pretty=oneline",  "-n",  "1" ], print_it=False )
+	log_values = log_res.split(" ", 1)
+	sha1 = log_values[0]
+	message = log_values[1]
+	
+	path = get_or_create_tmp_commit_folder(sha1)
+	print(path)
+	write_file(path + "/" + unpushed_commit_file_status, status)
--- hooks/hooks_files.py
+++ hooks/hooks_files.py
@@ -1,2 +1,32 @@
+import os
+from hooks_util import *
+from hooks_declare import *
 
 
+# Files
+
+def write_file(path, data, mode="a+"):
+	f = open(path, mode)
+	f.write(data)
+	f.close()
+
+# Folders
+
+def get_or_create_tmp_commit_folder(sha1):
+	path = get_or_create_tmp_folder() + "/" + sha1
+	
+	if not os.path.exists(path):
+		os.makedirs(path)
+	
+	return path
+
+def get_or_create_tmp_folder() :
+	path = get_root_directory() + "/" + unpushed_commit_folder
+	
+	if not os.path.exists(path):
+		os.makedirs(path)
+	
+	return path
+
+def get_root_directory():
+	return execute_cmd([ git_cmd, "rev-parse" ,"--show-toplevel"], print_it=False).strip()
