5ebd8f584b6fcaf9b04ffd070ddbace43de1e51d
FlorianDoublet <florian.doublet@hotmail.fr>
2016-10-05 21:21:37 +0200

    test

--- hooks/hooks_commit.py
+++ hooks/hooks_commit.py
@@ -41,6 +41,7 @@ def add_tmp_commit_files(status, tmp_folder_commit):
 def create_branch_for_commit(sha1):
 	execute_git_cmd([ "branch", sha1 ], False)
 
+
 def recreate_unpushed_commits():
 	#TODO : finir ca
 	
@@ -59,6 +60,7 @@ def recreate_unpushed_commits():
 		#copy all the file 
 		copyanything(unpushed_commit, get_root_directory())
 		#gerer les cas chiant PLUS TARD
+
 
 #return a hash of information for all the unpushed commit
 def parse_unpushed_commit_tmp():
